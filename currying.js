ec78b7622ed19afdc448270dfe2bc2d8940ef734
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2024-06-10 13:33:21 +0530

    curr

---
+++ currying.js
@@ -0,0 +1,21 @@
+// function sum(a){
+// return function(b){
+//     return function(c){
+//         return a + b + c
+//     }
+// }
+// }
+
+// console.log(sum(2)(6)(1))
+
+
+function operator(){
+
+return function(a){
+    return function(b){
+        
+    }
+}
+
+
+}
